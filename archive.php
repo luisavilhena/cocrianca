387eee71ccaecacfbc0b34603ec78c0c0eda05dd
luisavilhena <luisavilhena@gmail.com>
2024-04-22 20:58:37 -0300

    insere estrutura

--- archive.php
+++ archive.php
@@ -1,62 +1,71 @@
 <?php
-
-
-
-get_header(); ?>
-
-<?php 
-$args = array(
-	'post_type' => 'projetos',
-	'posts_per_page' => -1 
-);
-$the_query = new WP_Query( $args ); 
-$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$new_link = str_replace('projetos','projeto', $actual_link)
-
+    get_header();
+    $args = get_posts([
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'order'          => 'DES',
+        'orderby'        => 'date',
+        'posts_per_page' => -1,
+    ]);
+    $the_query = new WP_Query( $args ); 
+    echo $the_query;
 ?>
-	<div id="archive-projetos" class="structure-container">
-		<div class="carousel">
-			<div id="carousel-img">
-				<div class="item" style ="background-image: url('<?php echo get_template_directory_uri() ?>/resources/8.jpg');">
-				</div>				
-			</div>
-			<div class="carousel-text">
-				<p>
-					expericências coletivas de
-				</p>
-				<h1>
-					projeto, construção e aprendizagem
-				</h1>
-			</div>
-		</div>
-		<div class="structure-container__content structure-container__side">
-				 <?php $terms = get_terms(array(
-				 		'taxonomy'=>'projeto',
-				 		'hide_empty' => 'false',
-				 		'orderby'=> 'id',
-				 		'order' => 'ASC',
-				 )) ?>
-				<div class="filter">
-			    <?php foreach ( $terms as $term ) : ?>
-			        <a href="<?php echo $new_link.esc_attr( $term->slug )?>">
-			            <h3><?php echo $term->name ?></h3>
-			        </a>
-			    <?php endforeach; ?>
-			  </div>
-			<div class="cards-list">
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-					<a class="cards-list__item cards-list__item--special" href="<?php the_permalink(); ?>">
-						<div class="image-columns__item__img">
-							<?php the_post_thumbnail('horizontal-c'); ?>
-						</div>
-						<div class="cards-list__item-text">
-							<h5><?php the_excerpt();?></h5>
-							<h4><?php the_title();?></h4>
-						</div>
-					</a>
-				<?php endwhile; ?>
-			</div>
-		</div>
-	</div>
 
-<?php get_footer(); ?>
+<main id="blog-list" class="structure-container">
+    <div class="structure-container__all-content structure-container__side">
+        <?php
+            $latest_cpt = get_posts("post_type=post&numberposts=1");
+            $Id= $latest_cpt[0]->ID;
+            $Title= get_the_title($Id);
+            $Excerpt= get_the_excerpt($Id);
+            $Thumbnail=get_the_post_thumbnail($Id, 'horizontal-c');
+            $Author= get_the_author($Id);	
+            $Link = get_permalink($Id)			
+        ?>
+        <a href="<?php echo $Link;?>" class="blog-list__destak">
+            <div class="blog-list__destak__img"><?php echo $Thumbnail;?></div>
+            <div class="blog-list__destak__content">
+                <h4>Colunista</h4>
+                <h2><?php echo $Title;?></h2>
+                <p><?php echo $Excerpt;?></p>
+                <h3> por <?php echo $Author;?></h3>
+            </div>
+        </a>
+        <h2>leia outros artigos</h2>
+        <div class="cards-list">
+        <?php 
+            foreach ($the_query as $key => $value) {
+                $postId = $value->ID;
+                $url=get_permalink($value->ID);
+                $tags=get_the_tags($value->ID);
+                $thumbnail= get_the_post_thumbnail($value->ID, 'horizontal-c');
+                $title=$value->post_title;
+                if($postId==$Id){
+
+                }else {
+                    echo '
+                    <a class="cards-list__item cards-list__item--special" href="'.$url.'">
+                        <div class="image-columns__item__img">'.
+                            $thumbnail.'
+                        </div>
+                        <div class="cards-list__item-text">';
+                        if($tags){
+                            foreach($tags as $tag) {
+                            echo 
+                            '<h5>'.$tag->name .' </h5>'; 
+                        };
+                        }
+                    echo '	<h4>'.$title .'</h4>
+                        </div>
+                    </a>';
+                }
+            }
+        ?>
+        </div>
+    </div>
+    <?php the_content()?>
+</main>
+
+
+<?php
+get_footer();
